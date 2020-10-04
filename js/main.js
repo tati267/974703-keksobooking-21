53b9afa04b3e679e9f43a890b0a5e4a54769ec76
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-10-04 16:07:46 +1100

    переделки

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 'use strict';
 // module3-task1
-const QUANTITY_POSTS = 8;
+const POSTS_NUMBER = 8;
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const CHECKTIME = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+
+const PINS_AREA = document.querySelector(`.map__pins`);
+const map = document.querySelector(`.map`);
+map.classList.remove(`map--faded`);
 
 // случайное число в указаном диапазоне
 const getRandomInteger = (min, max) => {
@@ -14,130 +18,59 @@ const getRandomInteger = (min, max) => {
 const getRandomArrayElement = (arr) => {
   return arr[getRandomInteger(0, arr.length - 1)];
 };
+// Возвращает новый массив случайной длины
+const getRandomArr = (arr) => arr.slice(getRandomInteger(0, arr.length));
 
-// Генерация списка автаров
-const createAvatars = () => {
-  const avatars = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let avatar = `img/avatars/user0` + (i + 1) + `.png`;
-
-    avatars[i] = avatar;
-  }
-  return avatars;
-};
-
-// Генерация списка Titles
-const createTitles = () => {
-  const titles = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let title = `Заголовок предложения ` + (i + 1);
-
-    titles[i] = title;
-  }
-  return titles;
-};
-
-// Генерация списка адрессов
-const createAddresses = () => {
-  const addresses = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let x = getRandomInteger(100, 999);
-    let y = getRandomInteger(100, 999);
-    let address = x + `, ` + y;
-    addresses[i] = address;
-  }
-  return addresses;
-};
-
-// Генерация списка цен
-const createPrices = () => {
-  const prices = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let price = getRandomInteger(500, 5000);
-    prices[i] = price;
-  }
-  return prices;
-};
-
-// Генерация кол-во комнат
-const createRooms = () => {
-  const rooms = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let room = getRandomInteger(1, 20);
-    rooms[i] = room;
-  }
-  return rooms;
-};
-
-// Генерация кол-во гостей
-const createGuests = () => {
-  const guests = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let guest = getRandomInteger(0, 10);
-    guests[i] = guest;
-  }
-  return guests;
-};
-
-// Генерация местоположения
-const createLocations = () => {
-  const locations = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    let location = {};
-    location.x = getRandomInteger(0, 1200);
-    location.y = getRandomInteger(130, 630);
-    locations[i] = location;
-  }
-  return locations;
-};
-
-const avatars = createAvatars(QUANTITY_POSTS);
-const titles = createTitles(QUANTITY_POSTS);
-const addresses = createAddresses(QUANTITY_POSTS);
-const prices = createPrices(QUANTITY_POSTS);
-const rooms = createRooms(QUANTITY_POSTS);
-const guests = createGuests(QUANTITY_POSTS);
-const locations = createLocations(QUANTITY_POSTS);
-
-// генерация списка предложений
-const createPosts = () => {
-  const posts = [];
-  for (let i = 0; i < QUANTITY_POSTS; i++) {
-    const post = {
-      'author': {
-        'avatar': avatars[i]
+// Функция которая возвращает объекты
+const getPinAd = () => {
+  const makePost = [];
+  // Цикл который добавляет объекты
+  for (let i = 0; i < POSTS_NUMBER; i++) {
+    makePost.push({
+      author: {
+        avatar: `img/avatars/user0${i + 1}.png`
       },
-      'offers': {
-        'title': titles[i],
-        'address': addresses[i],
-        'price': prices[i],
-        'type': getRandomArrayElement(TYPES),
-        'rooms': rooms[i],
-        'guests': guests[i],
-        'checkin': getRandomArrayElement(CHECKTIME),
-        'checkout': getRandomArrayElement(CHECKTIME),
-        'features': getRandomArrayElement(FEATURES),
-        'description': `строка с описанием`,
-        'photos': getRandomArrayElement(PHOTOS)
+      offer: {
+        title: `Заголовок предложения`,
+        address: `x, y`,
+        price: getRandomInteger(1000, 1000000),
+        type: getRandomArrayElement(TYPES),
+        rooms: getRandomInteger(1, 5),
+        guests: getRandomInteger(1, 10),
+        checkin: getRandomArrayElement(CHECKTIME),
+        checkout: getRandomArrayElement(CHECKTIME),
+        features: getRandomArr(FEATURES),
+        description: `строка с описанием`,
+        photos: getRandomArrayElement(PHOTOS)
       },
-      'location': locations[i]
-    };
-    posts[i] = post;
+      location: {
+        x: getRandomInteger(0, map.clientWidth),
+        y: getRandomInteger(130, 630),
+      }
+    });
   }
-  return posts;
+  return makePost;
 };
 
-const posts = createPosts(QUANTITY_POSTS);
-const map = document.querySelector(`.map`);
-map.classList.remove(`map--faded`);
-const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+// Заполняет шаблон для отрисовки пина
+const createPinAd = (data) => {
+  const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+  const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < QUANTITY_POSTS; i++) {
-  let pin = pinTemplate.cloneNode(true);
+  data.forEach((item) => {
+    const pin = pinTemplate.cloneNode(true);
+    const img = pin.querySelector(`img`);
 
-  pin.style.left = (posts[i].location.x - 20) + `px`;
-  pin.style.top = (posts[i].location.y - 40) + `px`;
-  pin.querySelector(`img`).src = posts[i].author.avatar;
-  pin.querySelector(`img`).alt = posts[i].offers.title;
-  map.appendChild(pin);
-}
+    pin.style = `left: ${item.location.x - img.width / 2}px;
+                     top: ${item.location.y - img.height}px;`;
+    pin.querySelector(`img`).src = item.author.avatar;
+    pin.querySelector(`img`).alt = item.offer.title;
+    fragment.append(pin);
+  });
+
+  return fragment;
+};
+
+// Oтрисовывает пины в DOM
+const pinAd = getPinAd();
+PINS_AREA.append(createPinAd(pinAd));
